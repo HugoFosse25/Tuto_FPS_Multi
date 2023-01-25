f4cfcb5806a90eb7b25d16eab861c32c9ba0926a
Hugo Fossé <fosse.hugo25@gmail.com>
2023-01-25 20:57:44 +0100

    Intégration d'un joueur + son arme + calcul de sa vélocité

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]     //Le script ne peut pas fonctionner sans PlayerMotor
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private PlayerMotor motor;
+
+    private void Start() 
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        //Calculer la vélocité (vitesse) du mouvement de notre joueur
+        float _xMov = Input.GetAxisRaw("Horizontal");    //Raw pour enlever filtre de déplacement
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMov;
+        Vector3 _moveVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]     //Le script ne peut pas fonctionner sans Rigidbody
+public class PlayerMotor : MonoBehaviour
+{
+   
+}
