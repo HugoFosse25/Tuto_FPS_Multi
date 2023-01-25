ff67d55ae38b5f5d34de5938e6f52ce957b20eda
Hugo Fossé <fosse.hugo25@gmail.com>
2023-01-25 21:33:14 +0100

    Mouvement du personnage + Mouvement de la caméra

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,9 @@
 [RequireComponent(typeof(PlayerMotor))]     //Le script ne peut pas fonctionner sans PlayerMotor
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float mouseSensitivityX = 10f;
+    [SerializeField] private float mouseSensitivityY = 10f;
 
     private PlayerMotor motor;
 
@@ -15,12 +17,31 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         //Calculer la vélocité (vitesse) du mouvement de notre joueur
-        float _xMov = Input.GetAxisRaw("Horizontal");    //Raw pour enlever filtre de déplacement
+
+        float _xMov = Input.GetAxisRaw("Horizontal");    //Raw pour enlever lissage du déplacement
         float _zMov = Input.GetAxisRaw("Vertical");
 
         Vector3 _moveHorizontal = transform.right * _xMov;
         Vector3 _moveVertical = transform.forward * _zMov;
 
         Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        motor.Move(_velocity);  //On passe la vélocité a PlayerController pour qu'il éffectue réellemment le mouvement
+
+        //On calcule la rotation du joueur en un Vector3
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0, _yRot, 0) * mouseSensitivityX;
+
+        motor.Rotate(_rotation);    //On passe la rotation du joueur a PlayerController pour qu'il éffectue réellemment le mouvement
+
+        //On calcule la rotation de la caméra en un Vector3
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0, 0) * mouseSensitivityY; //On passe la rotation de la caméra a PlayerController pour qu'il éffectue réellemment le mouvement
+
+        motor.RotateCamera(_cameraRotation);
     }
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -3,5 +3,50 @@
 [RequireComponent(typeof(Rigidbody))]     //Le script ne peut pas fonctionner sans Rigidbody
 public class PlayerMotor : MonoBehaviour
 {
-   
+    [SerializeField]private Camera cam;
+
+   private Vector3 velocity;
+   private Vector3 rotation;
+   private Vector3 cameraRotation;
+   private Rigidbody rb;
+
+   private void Start()
+   {
+        rb = GetComponent<Rigidbody>();
+   }
+
+   public void Move(Vector3 _velocity)
+   {
+        velocity = _velocity;
+   }
+
+   public void Rotate(Vector3 _rotation)
+   {
+        rotation = _rotation;
+   }
+
+   public void RotateCamera(Vector3 _cameraRotation)
+   {
+        cameraRotation = _cameraRotation;
+   }
+
+   private void FixedUpdate()
+   {
+        PerformMovement();
+        PerformRotation();
+   }
+
+   private void PerformMovement()
+   {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
+        }
+   }
+
+   private void PerformRotation()
+   {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));  //Euler = Converti un Vector3 en Quaternion ; Rotation sur l'axe gauche droite
+        cam.transform.Rotate(-cameraRotation);  //
+   }
 }
